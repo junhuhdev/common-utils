4dc72a58e31326a039dcc53994351edf5892005f
Jun Huh <jun.huh@tele2.com>
2021-08-13 12:12:23 +0200

    Initial commit

---
+++ src/test/java/dev/huh/commonutils/CommonUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.huh.commonutils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonUtilsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
